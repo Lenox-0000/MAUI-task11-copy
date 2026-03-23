42c515e02dad3cc9dd07f37bca6afa660de72bbe
Adrian Zając <lenox.0000.0000@gmail.com>
2026-03-23 10:55:55 +0100

    sumarry

---
+++ MAUITask11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUITask11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUITask11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUITask11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
